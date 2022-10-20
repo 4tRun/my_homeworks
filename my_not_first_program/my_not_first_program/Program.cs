8b3a05c4a3f6914a4f59ac4cb441343efcb9f027
4tRun <vovanshum51@gmail.com>
2022-10-20 20:27:08 +0300

    My first program in GitHub!

---
+++ my_not_first_program/my_not_first_program/Program.cs
@@ -0,0 +1,10 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+        string output = $"Hello World and {name}!";
+        Console.WriteLine(output);
+    }
+}
