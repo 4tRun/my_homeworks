19e0b3a627112ce101c292f83836c6f7a1fea87e
Volodymyr Shum <vovanshum51@gmail.com>
2023-08-20 23:25:31 +0300

    lection phonebook

---
+++ lesson_9/lesson_9/PhonesStorage.cs
@@ -0,0 +1,100 @@
+﻿namespace lesson_9
+{
+    class PhonesStorage
+    {
+        private readonly string DbFilePath = @"D:\beetroot\my_homeworks\lesson_9\phoneRecords\phonedb.cvs";
+        private readonly char ColumnSeparator = ',';
+
+        public PhonesStorage()
+        {
+            EnsureDbFile();
+        }
+
+        public void Edit(int orderNumber)
+        {
+            var records = GetAllRecord();
+            var index = orderNumber - 1;
+
+            if(index < 0 || index > records.Length - 1)
+            {
+                var redBuffer = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Wrong order number.");
+                Console.ForegroundColor = redBuffer;
+            }
+
+            var recordObj = DeserializeRecord(records[index]);
+
+            var yellowBuffer = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("You are going to edit: ");
+            Print(recordObj);
+            Console.ForegroundColor = yellowBuffer;
+
+            var updateRecord = new PhoneRecord();
+
+            records[index] = SerializeRecord(updateRecord);
+
+            File.WriteAllLines(DbFilePath, records.Select(x => x.Trim()));
+        }
+        public void PrintAll()
+        {
+            var records = GetAllRecord();
+
+            for (int i = 0; i < records.Length; i++)
+            {
+                string? record = records[i];
+                var desirializedRecord = DeserializeRecord(record);
+                Print(desirializedRecord, i + 1);
+            }
+        }
+        public void Print(PhoneRecord phoneRecord)
+        {
+            Console.WriteLine($"+{phoneRecord.PhoneNumber} - {phoneRecord.FirstName} {phoneRecord.LastName}");
+        }
+
+        public void Print(PhoneRecord phoneRecord, int orderNumber)
+        {
+            Console.WriteLine($"[{orderNumber}]: +{phoneRecord.PhoneNumber} - {phoneRecord.FirstName} {phoneRecord.LastName}");
+        }
+
+        
+        public void Save(PhoneRecord phoneRecord)
+        {
+            EnsureDbFile();
+
+            File.AppendAllLines(DbFilePath, new[] { SerializeRecord(phoneRecord) });
+        }
+
+        private string[] GetAllRecord()
+        {
+            return File.ReadAllText(DbFilePath).Split('\n', StringSplitOptions.RemoveEmptyEntries);
+        }
+        private string SerializeRecord(PhoneRecord phoneRecord)
+        {
+            return $"{phoneRecord.FirstName}{ColumnSeparator}" +
+                $"{phoneRecord.LastName}{ColumnSeparator}" +
+                $"{phoneRecord.PhoneNumber}{ColumnSeparator}";
+        }
+
+        private PhoneRecord DeserializeRecord(string record)
+        {
+            string[] cellValues = record.Split(ColumnSeparator);
+
+            var parsedRecord = new PhoneRecord(cellValues[0], cellValues[1], cellValues[2]);
+
+            return parsedRecord;
+        }
+
+        private void EnsureDbFile()
+        {
+            if (!File.Exists(DbFilePath))
+            {
+                using (File.Create(DbFilePath))
+                {
+
+                }
+            }
+        }
+    }
+}
---
+++ lesson_9/lesson_9/Program.cs
@@ -0,0 +1,58 @@
+﻿using RandomNameGeneratorLibrary;
+using System.Security.Cryptography;
+
+namespace lesson_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("Choose operation (-g) - genarate, (-p) - print all, (-c) - create new name, (-e) - edit, (-out) - exit:");
+
+                string input = Console.ReadLine();
+
+                var storage = new PhonesStorage();
+
+                switch (input)
+                {
+                    case "-g":
+                        RunRandomGeneration();
+                        break;
+                    case "-c":
+                        storage.Save(new PhoneRecord());
+                        break;
+                    case "-p":
+                        storage.PrintAll();
+                        break;
+                    case "-e":
+                        storage.Edit(int.Parse(Console.ReadLine()));
+                        break;
+                    case "-out":
+                        return;
+                    default:
+                        continue;
+                }
+            } while (true);
+        }
+
+        private static void RunRandomGeneration()
+        {
+            var placeGenerator = new PersonNameGenerator();
+            var randomNumberGenerator = new Random();
+            var storage = new PhonesStorage();
+
+            for (var i =0; i < 10; i++)
+            {
+                var newRecord = new PhoneRecord(placeGenerator.GenerateRandomFirstName(),
+                    placeGenerator.GenerateRandomLastName(),
+                    $"{randomNumberGenerator.Next(38010, 38099)}" +
+                        $"{randomNumberGenerator.Next(1111111, 9999999)}");
+
+                storage.Save(newRecord);
+                
+            }
+        }
+    }
+}
